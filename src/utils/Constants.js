a668ceac939040e50c395199b4ffbca45bfc2374
haseeb-kp <haseebkph@gmail.com>
2023-05-06 16:56:56 +0530

    updated

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,5 +1,6 @@
 // export const baseUrl = "http://127.0.0.1:8000/api/v1/";
-export const baseUrl = "http://3.108.190.246:8000/api/v1/";
+// export const baseUrl = "http://3.108.190.246:8000/api/v1/";
+export const baseUrl = "https://fetllabackend:8000/api/v1/";
 
 // urls
 export const loginPost = "auth/login/";
