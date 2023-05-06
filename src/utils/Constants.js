130ba6a41d144b96d285c6bced3083f6d5714f47
haseeb-kp <haseebkph@gmail.com>
2023-05-06 16:04:30 +0530

    changed baseUrl

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,4 +1,5 @@
-export const baseUrl = "http://127.0.0.1:8000/api/v1/";
+// export const baseUrl = "http://127.0.0.1:8000/api/v1/";
+export const baseUrl = "http://3.108.190.246:8000/api/v1/";
 
 // urls
 export const loginPost = "auth/login/";
