f3fbc5cb3ff6def70780efe6d4339bab9489a4fb
haseeb-kp <haseebkph@gmail.com>
2023-05-07 12:22:11 +0530

    updated baseUrl

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,6 +1,6 @@
 // export const baseUrl = "http://127.0.0.1:8000/api/v1/";
 // export const baseUrl = "http://3.108.190.246:8000/api/v1/";
-export const baseUrl = "https://fetllabackend:8000/api/v1/";
+export const baseUrl = "https://fetlla.up.railway.app/api/v1/";
 
 // urls
 export const loginPost = "auth/login/";
